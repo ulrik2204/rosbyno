531a77a038fc6bd8bb4b92c06ca0824b31156e0c
Ulrik Røsby <ulrik@rosby.no>
2022-09-10 18:19:22 +0200

    feat: add Line component

---
+++ src/components/Line.tsx
@@ -0,0 +1,14 @@
+import { Box } from "@mui/material";
+import { ReactNode } from "react";
+
+type LineProps = {
+  children?: ReactNode;
+};
+export default function Line(props: LineProps) {
+  return (
+    <Box sx={{ display: "flex", alignItems: "center", gap: "0.5em" }}>
+      <Box sx={{ width: "3em", height: "0.15em", backgroundColor: "primary.main" }}></Box>
+      {props.children}
+    </Box>
+  );
+}
