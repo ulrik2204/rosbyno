9eb7ea4c99dab1a178b6b16edbcf4f9f8dede26a
Ulrik Røsby <ulrik@rosby.no>
2022-09-10 18:17:59 +0200

    feat: add title and text fonts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -16,6 +16,15 @@ const theme = createTheme({
       default: "#C1EFFF",
     },
   },
+  typography: {
+    fontFamily: "'Open Sans', sans-serif",
+    h1: { fontFamily: "'Montserrat', sans-serif", fontSize: 64, fontWeight: 500 },
+    h2: { fontFamily: "Montserrat, sans-serif", fontSize: 32, fontWeight: 400 },
+    h3: { fontFamily: "Montserrat, sans-serif", fontSize: 24, fontWeight: 400 },
+    h4: { fontFamily: "Montserrat, sans-serif", fontSize: 20, fontWeight: 400 },
+    h5: { fontFamily: "Montserrat, sans-serif" },
+    h6: { fontFamily: "Montserrat, sans-serif" },
+  },
 });
 
 function MyApp({ Component, pageProps }: AppProps) {
