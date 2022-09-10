4b83f1a20a2d50771d77fc390e48b4cac9f245c7
Ulrik Røsby <ulrik@rosby.no>
2022-09-10 18:20:04 +0200

    feat: add image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,19 @@
-import { Button } from "@mui/material";
+import { Button, Typography } from "@mui/material";
 import type { NextPage } from "next";
+import Line from "../components/Line";
+import Picture from "../components/Picture";
 
 const Home: NextPage = () => {
   return (
     <div>
       <div className="text-3xl font-bold underline">Some text</div>
+      <Picture
+        src="/images/seawiz_uten_bg.png"
+        alt="Picture of me"
+        sx={{ width: 200, height: 200 }}
+      />
+      <Line>Hello</Line>
+      <Typography variant="h1">Ulrik Røsby</Typography>
       <Button onClick={() => console.log("Button click")} variant="contained" color="primary">
         Hello
       </Button>
