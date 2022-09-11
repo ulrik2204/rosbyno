a57a0c6f5eb395848b39467f7f185405ac998738
Ulrik Røsby <ulrik@rosby.no>
2022-09-11 20:50:30 +0200

    feat: create IconLink component

---
+++ src/components/IconLink.tsx
@@ -0,0 +1,27 @@
+import { Box, Typography } from "@mui/material";
+import Image from "next/image";
+
+type IconLinkProps = {
+  src: string;
+  href: string;
+  text?: string;
+};
+export default function IconLink(props: IconLinkProps) {
+  return (
+    <Box width="64px">
+      <a href="https://github.com/ulrik2204">
+        <Box
+          sx={{
+            display: "flex",
+            flexFlow: "column",
+            alignItems: "center",
+            width: "64px",
+          }}
+        >
+          <Image src={props.src} alt="" objectFit="contain" layout="fixed" width={64} height={64} />
+          <Typography>{props.text}</Typography>
+        </Box>
+      </a>
+    </Box>
+  );
+}
