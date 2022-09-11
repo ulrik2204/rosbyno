e2e1922e630cb8411f776cff759e04681b2a55e5
Ulrik Røsby <ulrik@rosby.no>
2022-09-11 13:48:00 +0200

    fix: remove unnecessary assets and code related to wave

--- src/components/ThreeHeader/ThreeHeader.tsx
+++ src/components/ThreeHeader/ThreeHeader.tsx
@@ -22,11 +22,11 @@ import styles from "./ThreeHeader.module.css";
  *
  */
 
-export class ThreeHeader extends React.Component<{}, { isMouseOverCanvas: boolean }> {
+export class ThreeHeader extends React.Component<{}, { isCursorOverCanvas: boolean }> {
   constructor(props: {}) {
     super(props);
     this.state = {
-      isMouseOverCanvas: false,
+      isCursorOverCanvas: false,
     };
   }
   scene = new THREE.Scene();
@@ -42,78 +42,22 @@ export class ThreeHeader extends React.Component<{}, { isMouseOverCanvas: boolea
     x: 0.5,
     y: 0.5,
   };
-  textures: any = {};
-  firstMaterial: any = null;
-  secondMaterial: any = null;
+  firstMaterial: THREE.ShaderMaterial | null = null;
+  secondMaterial: THREE.ShaderMaterial | null = null;
 
   // Geometries
   planeGeometry = new THREE.PlaneGeometry(10, 300, 1, 100);
 
-  debugObject = {
-    cameraPosition: { x: 0, y: 0, z: 2 },
-  };
-
   timeline = typeof window !== "undefined" ? anime.timeline() : null;
 
-  initScene(texture: any) {
-    const colors = [
-      "rgb(40,110,240)",
-      "rgb(89,227,221)",
-      "rgb(241,209,89)",
-      "rgb(5,143,219)",
-      "rgb(249,80,117)",
-      "rgb(35,88,241)",
-      "rgb(76,182,253)",
-      "rgb(63,255,167)",
-      "rgb(6,184,219)",
-      "rgb(245,177,99)",
-      "rgb(31,133,238)",
-      "rgb(66,236,181)",
-      "rgb(101,217,209)",
-      "rgb(82,134,245)",
-      "rgb(53,222,179)",
-      "rgb(36,174,251)",
-      "rgb(73,218,228)",
-      "rgb(29,178,255)",
-    ];
-
-    const canvasPattern0 = document.createElement("canvas");
-    const canvasPattenSize0 = 1024;
-    canvasPattern0.width = this.sizes.width;
-    canvasPattern0.height = this.sizes.height;
-    const canvasPatternContext1 = canvasPattern0.getContext("2d");
-    const circleCount = 100;
-    for (let i = 0; i < circleCount; i++) {
-      const cx = Math.floor(Math.random() * canvasPattenSize0);
-      let cy = Math.floor(Math.random() * canvasPattenSize0);
-      const width = 2;
-      let height = 10;
-
-      cy = 0;
-      height = canvasPattenSize0;
-
-      if (canvasPatternContext1) {
-        canvasPatternContext1.fillStyle = colors[Math.floor(Math.random() * colors.length)];
-        canvasPatternContext1.fillRect(cx, cy, width, height);
-      }
-    }
+  initScene() {
     const geometry = new THREE.PlaneGeometry(10, 300, 1, 100);
-
-    const texture0 = new THREE.Texture(canvasPattern0);
-    texture0.needsUpdate = true;
-
     this.firstMaterial = new THREE.ShaderMaterial({
       transparent: true,
       fragmentShader,
       vertexShader: firstVertexShader,
       side: THREE.DoubleSide,
       uniforms: {
-        u_texture: {
-          value: texture0,
-        },
-        u_textureSize: {
-          value: canvasPattenSize0,
-        },
         u_dimensions: {
           value: new THREE.Vector2(this.sizes.width, this.sizes.height),
         },
@@ -138,12 +82,6 @@ export class ThreeHeader extends React.Component<{}, { isMouseOverCanvas: boolea
     this.secondMaterial = new THREE.ShaderMaterial({
       transparent: true,
       uniforms: {
-        texture: {
-          value: texture0,
-        },
-        u_textureSize: {
-          value: canvasPattenSize0,
-        },
         u_dimensions: {
           value: new THREE.Vector2(this.sizes.width, this.sizes.height),
         },
@@ -169,29 +107,11 @@ export class ThreeHeader extends React.Component<{}, { isMouseOverCanvas: boolea
     if (this.renderer) {
       this.renderer.setSize(this.sizes.width, this.sizes.height);
       this.renderer.setClearColor(new THREE.Color("#C1EFFF"));
-      // this.renderer.setClearColor(new THREE.Color("#FFFFFF"));
     }
     this.scene.add(mesh);
     this.scene.add(mesh2);
 
     // Normal timeline
-    this.timeline.add({
-      targets: [`.${styles.heading}`],
-      opacity: 1,
-      duration: 1000,
-      easing: "easeInOutQuart",
-    });
-
-    this.timeline.add(
-      {
-        targets: [`.${styles.ctas}`],
-        opacity: 1,
-        duration: 1000,
-        easing: "easeInOutQuart",
-      },
-      300,
-    );
-
     this.timeline.add(
       {
         targets: [this.firstMaterial.uniforms.u_opacity, this.secondMaterial.uniforms.u_opacity],
@@ -203,63 +123,40 @@ export class ThreeHeader extends React.Component<{}, { isMouseOverCanvas: boolea
     );
   }
 
-  initDebug() {
-    this.gui = new GUI();
-
-    const cameraFolder = this.gui.addFolder("Camera");
-    cameraFolder.add(this.debugObject.cameraPosition, "x").min(-5).max(5).step(0.001);
-    cameraFolder.add(this.debugObject.cameraPosition, "y").min(-5).max(5).step(0.001);
-    cameraFolder.add(this.debugObject.cameraPosition, "z").min(-5).max(5).step(0.001);
-
-    cameraFolder.onChange(() => {
-      const { x, y, z } = this.debugObject.cameraPosition;
-      this.camera.position.set(x, y, z);
-    });
-  }
-
   componentDidMount() {
     const canvasEl = document.querySelector(`.${styles.canvas}`);
     this.renderer = new THREE.WebGLRenderer({
       antialias: true,
       canvas: canvasEl ?? undefined,
     });
-    const textureLoader = new THREE.TextureLoader();
-
-    textureLoader.load("/images/ulrik_seawiz_uten_bg.png", (texture) => {
-      texture.needsUpdate = true;
-      this.initScene(texture);
-      // this.initDebug();
-      this.tick();
-    });
-
+    this.initScene();
+    this.tick();
     window.addEventListener("resize", this.onResize.bind(this));
     window.addEventListener("mousemove", this.handleMouseMove.bind(this));
     window.addEventListener("touchmove", this.handleTouchMove.bind(this));
-  }
-  componentDidUpdate(prevProps: Readonly<{}>, prevState: Readonly<{}>, snapshot?: any): void {
-    console.log("Hello");
-    console.log(this.state);
   }
 
   tick() {
     const elapsedTime = this.clock.getElapsedTime();
 
-    this.firstMaterial.uniforms.u_mousePosition.value = new THREE.Vector2(
-      this.mousePosition.x,
-      this.mousePosition.y,
-    );
+    if (this.firstMaterial) {
+      this.firstMaterial.uniforms.u_mousePosition.value = new THREE.Vector2(
+        this.mousePosition.x,
+        this.mousePosition.y,
+      );
 
-    this.firstMaterial.uniforms.u_time.value = elapsedTime / 3;
+      this.firstMaterial.uniforms.u_time.value = elapsedTime / 3;
+    }
+    if (this.secondMaterial) {
+      this.secondMaterial.uniforms.u_mousePosition.value = new THREE.Vector2(
+        this.mousePosition.x,
+        this.mousePosition.y,
+      );
 
-    this.secondMaterial.uniforms.u_mousePosition.value = new THREE.Vector2(
-      this.mousePosition.x,
-      this.mousePosition.y,
-    );
+      this.secondMaterial.uniforms.u_time.value = elapsedTime / 3;
+    }
 
-    this.secondMaterial.uniforms.u_time.value = elapsedTime / 3;
-
-    const isMobile = false;
-
+    const isMobile = false; // window.innerWidth < 750;
     this.camera.position.x +=
       ((this.mousePosition.x - 0.5) * (isMobile ? 140 : 40) - this.camera.position.x) * 0.01;
     this.camera.position.y +=
@@ -273,7 +170,7 @@ export class ThreeHeader extends React.Component<{}, { isMouseOverCanvas: boolea
 
   // Handlers
   onResize() {
-    // Update sizes
+    // Update sizes, only care about updating width
     this.sizes.width = window.innerWidth;
 
     // Update camera
@@ -288,14 +185,15 @@ export class ThreeHeader extends React.Component<{}, { isMouseOverCanvas: boolea
   }
 
   handleTouchMove(e: TouchEvent) {
-    if (!this.state.isMouseOverCanvas) return;
+    if (!this.state.isCursorOverCanvas) return;
+    e.preventDefault();
     const touch = e.touches[0];
     this.mousePosition.x = touch.clientX / this.sizes.width;
     this.mousePosition.y = touch.clientY / this.sizes.height;
   }
 
   handleMouseMove(e: MouseEvent) {
-    if (!this.state.isMouseOverCanvas) return;
+    if (!this.state.isCursorOverCanvas) return;
     this.mousePosition.x = e.clientX / (window.innerWidth + 1);
     this.mousePosition.y = e.clientY / (window.innerHeight + 1);
   }
@@ -303,13 +201,13 @@ export class ThreeHeader extends React.Component<{}, { isMouseOverCanvas: boolea
   render() {
     return (
       <div
-        onMouseEnter={() => this.setState({ isMouseOverCanvas: true })}
-        onMouseLeave={() => this.setState({ isMouseOverCanvas: false })}
+        onMouseEnter={() => this.setState({ isCursorOverCanvas: true })}
+        onMouseLeave={() => this.setState({ isCursorOverCanvas: false })}
+        onTouchStart={() => this.setState({ isCursorOverCanvas: true })}
+        onTouchEnd={() => this.setState({ isCursorOverCanvas: false })}
         className={styles.wrapper}
       >
-        <div className={styles.reference}></div>
-        <div className={styles.header}>
-          <div className={styles.ctas}></div>
+        <div className={styles.canvasDiv}>
           <canvas className={styles.canvas} />
         </div>
       </div>
